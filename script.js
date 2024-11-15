7143b30539269f3537cd986653ebc27f4f201b1a
masamasaowl <aryancheers@gmail.com>
2024-11-15 19:45:49 +0530

    completed the CSS

---
+++ script.js
@@ -0,0 +1,5 @@
+// we want three things to happen
+// 1. All countries must be listed in the dropdown
+// 2. Flag must chnage with the country
+// 3. Accurate currency exchange rate must be displayed
+
